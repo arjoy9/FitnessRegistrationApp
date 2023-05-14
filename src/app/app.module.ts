6f0b28dcb7c69bf1e58c67912bd931839fd9fc5f
arjoy9 <gctljoy@gmail.com>
2023-05-14 18:27:31 +0600

    view records

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -46,7 +49,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatRadioModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration-list/registration-list.component.ts
+++ src/app/registration-list/registration-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+// import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { user } from '../models/user.model';
+import { ApiService } from '../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-list',
   templateUrl: './registration-list.component.html',
   styleUrls: ['./registration-list.component.scss']
 })
-export class RegistrationListComponent {
+export class RegistrationListComponent implements OnInit {
+
+  public dataSource!: MatTableDataSource<user>;
+  public users!:user[];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  displayedColumns:string[] = ['id','firstName','lastName','email','mobile','bmiResult','gender','package','enquiryDate','action']
+
+  constructor(private api:ApiService, private router:Router){}
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(){
+    this.api.getRegisteredUser().subscribe(result=>{
+      this.users=result;
+      this.dataSource=new MatTableDataSource(this.users);
+      this.dataSource.paginator=this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  edit(id:number){
+    this.router.navigate(['update',id]);
+  }
 
 }
